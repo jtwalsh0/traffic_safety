13beed268ba20b17d0d446fa341501345a5e3881
Joe Walsh <joewalsh@Joes-MacBook-Pro.local>
2017-10-28 19:30:43 -0500

    create data directory structure

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# folder structure
+WORKDIR=$(jq '.working_directory' < credentials.json | tr -d '"')
+parallel 'mkdir -p {1}/{2}/{3}' ::: "$WORKDIR" ::: 'raw' 'preprocessed' ::: 'ALDOT' 'CAPS'
+
 # read database credentials
 PGHOST=$(jq '.db.PGHOST' < credentials.json | tr -d '"')
 PGUSER=$(jq '.db.PGUSER' < credentials.json | tr -d '"')
@@ -9,6 +13,6 @@ PGDATABASE=$(jq '.db.PGDATABASE' < credentials.json | tr -d '"')
 PGPORT=$(jq '.db.PGPORT' < credentials.json | tr -d '"')
 
 # run db creation scripts
-PGPASSWORD="$PGPASSWORD" psql -X -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -f infrastructure/db/create_matrices_schema.sql
-PGPASSWORD="$PGPASSWORD" psql -X -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -f infrastructure/db/create_modeling_schema.sql
-PGPASSWORD="$PGPASSWORD" psql -X -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -f infrastructure/db/create_results_schema.sql
+PGPASSWORD="$PGPASSWORD" psql -X -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -p "$PGPORT" -f infrastructure/db/create_matrices_schema.sql
+PGPASSWORD="$PGPASSWORD" psql -X -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -p "$PGPORT" -f infrastructure/db/create_modeling_schema.sql
+PGPASSWORD="$PGPASSWORD" psql -X -h "$PGHOST" -U "$PGUSER" -d "$PGDATABASE" -p "$PGPORT" -f infrastructure/db/create_results_schema.sql
